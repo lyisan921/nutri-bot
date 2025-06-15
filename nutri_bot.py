c1276e5450bc9d0b7c6192d1ee9565ecfc062b90
lyisan921 <liaisan241@gmail.com>
2025-06-15 21:30:03 +0200

    Update nutri_bot.py

--- nutri_bot.py
+++ nutri_bot.py
@@ -1,13 +1,13 @@
+# nutri_bot.py
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-TOKEN = "5284761727:AAG5nQPZNpWLN4Gc3fCpYGtGBT83wYLNK0U"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой нутри-бот 🤖")
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = ApplicationBuilder().token("5284761727:AAG5nQPZNpWLN4Gc3fCpYGtGBT83wYLNK0U").build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
